cf025daddae63169a4ec93b824cb2e5735ec9f06
Pamela Alonso <pcalonsowebsite@gmail.com>
2021-12-06 20:13:13 +0100

    .Cambios

--- src/app/auth/pages/main/main.component.ts
+++ src/app/auth/pages/main/main.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styles: [
+  styleUrls: ["./main.component.css"
   ]
 })
 export class MainComponent implements OnInit {
